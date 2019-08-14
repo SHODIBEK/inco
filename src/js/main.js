e3fd517dbca66df7a6b5935342f49f51335c5935
SHODIBEK <mr.agape@mail.ru>
2019-08-15 02:48:28 +0500

    index mobile

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,10 @@ $(document).ready(() => {
     let productSlider = $("#product-carousel");
     let advantageSlider = $("#advantage-carousel");
     let advantageThumb = $('#advantage-thumb');
-    let newsTitle = $('.news-item-title');
+    let mobileSubmenu = $('.mobile-menu .btn-submenu');
+    let mobileMenu = $('#menu');
+    let advantageCarousel = $('#advantage-slider');
+    let newsSlider = $('#news-slider');
 
     submenu.on('click', (e) => {
         e.preventDefault();
@@ -52,12 +55,20 @@ $(document).ready(() => {
     });
 
     productSlider.owlCarousel({
-        items: 6,
         loop: true,
         nav: true,
-        dots: false,
         margin: 20,
         navText: ['<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 7.5H1M1 7.5L7.5 1M1 7.5L7.5 14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 7.5L14 7.5M14 7.5L7.5 14M14 7.5L7.5 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'],
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+            },
+            1024: {
+                items: 6,
+                dots: false,
+            }
+        },
     });
 
     advantageSlider.owlCarousel({
@@ -78,4 +89,35 @@ $(document).ready(() => {
     advantageSlider.on('changed.owl.carousel', function(e) {
         advantageThumb.children().eq(e.page.index).addClass('active').siblings().removeClass('active');
     });
+
+    mobileSubmenu.on('click', function() {
+        var _this = $(this);
+        _this.toggleClass('btn-yellow');
+        _this.next().slideToggle();
+    });
+
+    mobileMenu.on('click', function() {
+        var _this = $(this);
+        _this.toggleClass('mobile-menu-btn--open');
+        $('.js-menu-toggle').slideToggle();
+        $('header').toggleClass('header-open');
+    });
+
+    if ($window.width() < 1024) {
+        advantageCarousel.owlCarousel({
+            items: 1,
+            loop: true,
+            nav: true,
+            dots: true,
+            navText: ['<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 7.5H1M1 7.5L7.5 1M1 7.5L7.5 14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 7.5L14 7.5M14 7.5L7.5 14M14 7.5L7.5 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'],
+        });
+
+        newsSlider.owlCarousel({
+            items: 1,
+            loop: true,
+            nav: true,
+            dots: true,
+            navText: ['<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 7.5H1M1 7.5L7.5 1M1 7.5L7.5 14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 7.5L14 7.5M14 7.5L7.5 14M14 7.5L7.5 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'],
+        });
+    }
 });
